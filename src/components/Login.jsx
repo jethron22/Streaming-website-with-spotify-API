162a2545ab98f7a0ede851224105bb781ae63748
Jethron <jethronbwira@gmail.com>
2022-09-02 10:57:55 +0200

    feat: displaying albums items cards with react-b

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,73 +1,71 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
-import Search from './Search';
-import { FaCircle } from 'react-icons/fa'; 
-import { FaDrum } from 'react-icons/fa'; 
-import Deconnexion from './Deconnexion';
-import Mainmusics from './Mainmusics';
-
+import Search from "./Search";
+import { FaCircle } from "react-icons/fa";
+import { FaDrum } from "react-icons/fa";
+import Deconnexion from "./Deconnexion";
+import Mainmusics from "./Mainmusics";
 
 function Login() {
-const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
-function handleCallbackResponse(response) {
+  function handleCallbackResponse(response) {
     const userObject = jwt_decode(response.credential);
-    setUser(userObject) 
+    setUser(userObject);
     document.getElementById("signInDiv").hidden = true;
   }
 
   useEffect(() => {
     /* global Google initialize sikiliza-app-stream*/
     google.accounts.id.initialize({
-          client_id: "910275264123-fldtjgd3317ec0d76ipmtabo4n0c5u64.apps.googleusercontent.com",
-          callback: handleCallbackResponse
-        });
+      client_id:
+        "910275264123-fldtjgd3317ec0d76ipmtabo4n0c5u64.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
 
-    google.accounts.id.renderButton(
-    document.getElementById("signInDiv"),
-      { theme: "Outline", size: "larger" }
-    )
-
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "Outline",
+      size: "larger",
+    });
   }, []);
   console.log(user);
 
   return (
     <div>
-    <div className='Login'>
-    
-      <div id="signInDiv">
-    
-    </div>
-    
-      {user &&
-       
-          <div className='userIds'>
-            <div className='profileAndlogo'>
-            <span className='LOGO'> <FaDrum className='Drum'/>Sikiliza stream</span>
-            <img className='pictureId' src={user.picture} />
+      <div className="Login">
+        <div id="signInDiv"></div>
+
+        {user && (
+          <div className="userIds">
+            <div className="profileAndlogo">
+              <span className="LOGO">
+                {" "}
+                <FaDrum className="Drum" />
+                Sikiliza stream
+              </span>
+              <img className="pictureId" src={user.picture} />
             </div>
-            <div className='Mysearch'>
-          <Search /> 
-          </div>
-          <div className='connect'>
-          <span className='enTantque'>connecté <FaCircle className='Circle' /></span>
-         
-            <h3 className='userNameId'>{user.name} </h3>
+            <div className="Mysearch">
+              <Search />
+            </div>
+            <div className="connect">
+              <span className="enTantque">
+                connecté <FaCircle className="Circle" />
+              </span>
+
+              <h3 className="userNameId">{user.name} </h3>
             </div>
             <Deconnexion />
           </div>
-      
-      }
+        )}
       </div>
 
-      <div id='mainHere'>
-        <p><Mainmusics /></p>
+      <div id="mainHere">
+          <Mainmusics />
       </div>
     </div>
-    
-
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/components/Mainmusics.jsx
+++ src/components/Mainmusics.jsx
@@ -1,49 +1,56 @@
 import React from "react";
-import '../App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row } from "react-bootstrap";
-import {Card} from 'react-bootstrap';
-import{useState, useEffect } from 'react'
-
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Card,
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 function Mainmusics() {
-
-const [searchInput, setSearchInput] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
   return (
     <div>
-        <container>
-            <InputGroup className="mb-3" size="lg">
-<FormControl placeholder="Rechercher un artiste"
-type="Input" 
-onKeyDown={ event => {
-    if (event.key == "Enter" ) {
-      console.log("pressed enter");
-    }
-}}
-
-
-onChange={event => setSearchInput(event.target.value)}
-
-
-/>
-<button type="button" class="btn btn-primary" onClick={()=>{console.log("hello world")}}>
-rechercher
-</button>
-
-            </InputGroup>
-        </container>
-        <Container>
-<Card>
-  <Card src="#" />
-  <Button.Body>
-    <Card.Title> Nom de l'album</Card.Title>
-  </Button.Body>
-</Card>
-</Container>
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Rechercher un artiste"
+            type="Input"
+            onKeyDown={(event) => {
+              if (event.key == "Enter") {
+                console.log(searchInput);
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => {
+              console.log("hello world");
+            }}
+          >
+            rechercher
+          </button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          <Card>
+            <Card src="#" />
+            <Button>
+              <Card.Title> Nom de l'album</Card.Title>
+            </Button>
+          </Card>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Mainmusics
-
+export default Mainmusics;
