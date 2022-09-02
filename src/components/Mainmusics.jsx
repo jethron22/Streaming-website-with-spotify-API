89fee8b7075ffaf2db6b50283e7c90da50a75668
Jethron <jethronbwira@gmail.com>
2022-09-02 11:18:43 +0200

    feat: refactor

--- src/components/Mainmusics.jsx
+++ src/components/Mainmusics.jsx
@@ -1,39 +1,40 @@
 import React from "react";
-import '../App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row, card } from "react-bootstrap";
-import{useEffect, useState} from 'react'
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Container,InputGroup,FormControl,Button, Row, Card} from "react-bootstrap";
 
+import { useEffect, useState } from "react";
 
 function Mainmusics() {
-
-const [searchInput, setSearchInput] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
   return (
     <div>
-        <container>
-            <InputGroup className="mb-3" size="lg">
-<FormControl placeholder="Rechercher un artiste"
-type="Input" 
-onKeyPress={ event => {
-    if (event.Key == "Enter" ) {
-        console.log("press enter");
-    }
-}}
-onchange={event => setSearchInput(event.target.value)}
+      <container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Rechercher un artiste"
+            type="Input"
+            onKeyPress={(event) => {
+              if (event.Key == "Enter") {
+                console.log(searchInput);
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <button
+            onClick={() => {
+              console.log("hello world");
+            }}
+          >
+            rechercher
+          </button>
+        </InputGroup>
+		   </container>
+		  <Card></Card>
 
-/>
-<button onClick={()=>{console.log("hello world")}}>
-rechercher
-</button>
-
-            </InputGroup>
-        </container>
-
-    
     </div>
-  )
+  );
 }
 
-export default Mainmusics
-
+export default Mainmusics;
