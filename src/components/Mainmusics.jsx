014138e247d6334b44bd3b0e78b79c220ce643cf
Jethron <jethronbwira@gmail.com>
2022-09-05 13:41:52 +0200

    feat: displaying artists, albums, and images

--- src/components/Mainmusics.jsx
+++ src/components/Mainmusics.jsx
@@ -1,66 +1,92 @@
 import React from "react";
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Card, Container, InputGroup, FormControl, Button, Row} from"react-bootstrap";
+import {
+  Card,
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+} from "react-bootstrap";
 import { useState, useEffect } from "react";
 
 const CLIENT_ID = "3ef00953d1fb46f8a315b8b4f9141d86";
 const CLIENT_SECRET = "1dda9ed5c4ff447fbfd0b8d560fce93f";
-	
 
 function Mainmusics({}) {
-	const [searchInput, setSearchInput] = useState("koffi");
-	const [accessToken, setAccessToken] = useState("")
+  const [searchInput, setSearchInput] = useState("koffi");
+  const [accessToken, setAccessToken] = useState("");
+  const [dataAlbum, setDataAlbum] = useState([]);
+  const [artistID, setArtistID] = useState(null);
 
-	useEffect(() => {
+  useEffect(() => {
+    let authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": " application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    };
 
-		let authParameters = {
-			method: 'POST',
-			headers: {
-				'Content-Type': ' application/x-www-form-urlencoded'
-				
-			},
-			body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-		}
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+        console.log(data);
+        search();
+      });
+  }, [artistID]);
 
-		fetch("https://accounts.spotify.com/api/token", authParameters).then(result => result.json()).then(data => {
-			setAccessToken(data.access_token)
-			console.log(data);
-			search()
-		})
-	
-	}, []);
-	
-    // Système de Recherche des musiques
+  // Système de Recherche des musiques
 
-	async function search () {
-		console.log("Trouver" + searchInput);
-		// Trouver l'ID de l'artiste au moyen de la recherche implementé//
+   function search() {
+    console.log("Trouver" + searchInput);
+    // Trouver l'ID de l'artiste au moyen de la recherche implementé//
 
-		let parametresRecherche= {
-			method: 'GET',
-			headers: {
-				'content-type': 'application/json',
-				Authorization : 'Bearer  ' + accessToken
-			}
-		}
+    let parametresRecherche = {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer  " + accessToken,
+      },
+    };
 
-		console.log(accessToken)
+    console.log(accessToken);
 
-		let artistID = fetch("https://api.spotify.com/v1/search?q=" + searchInput + '&type=artist', parametresRecherche).then(response => response.json()).then(data => { return data.artists.items[0].id })
-		
-		console.log("Artist ID is" + artistID);
+    fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      parametresRecherche
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        /*return data.artists.items[0].id*/
+        setArtistID(data.artists.items[0].id);
+      });
 
-		let album = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50').then(response => response.json()).then(data => {
-			console.log();
-		})
+    //console.log();
 
-		// Afficher les albums d'artistes aux utilisateurs //
-		
-	};
+    fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=album",
+      parametresRecherche
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setDataAlbum(data.albums.items);
+	  });
+	   
+	 
 
+    // Afficher les albums d'artistes aux utilisateurs //
+  }
+  console.log(artistID);
 
-	return (
+  return (
     <div>
       <Container>
         <InputGroup className="mb-3" size="lg">
@@ -69,49 +95,28 @@ function Mainmusics({}) {
             type="Input"
             onKeyDown={(event) => {
               if (event.key == "Enter") {
-                search()
+                search();
               }
             }}
             onChange={(event) => setSearchInput(event.target.value)}
           />
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={search}>
+          <button type="button" className="btn btn-primary" onClick={search}>
             rechercher
           </button>
         </InputGroup>
       </Container>
       <Container>
         <Row className="mx-2 row row-cols-4">
-          <Card>
-            <Card src="#" />
-            <Card.Body>
-              <Card.Title> Nom de l'album</Card.Title>
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card src="#" />
-            <Card.Body>
-              <Card.Title> Nom de l'album</Card.Title>
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card src="#" />
-            <Card.Body>
-              <Card.Title> Nom de l'album</Card.Title>
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card src="#" />
-            <Card.Body>
-              <Card.Title> Nom de l'album</Card.Title>
-            </Card.Body>
-		</Card>
-					
+          {dataAlbum.map((album, i) => {
+            return (
+              <Card>
+                <img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </Row>
       </Container>
     </div>
